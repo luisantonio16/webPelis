272e34040695436a5c7f69b05dbe9b16530e7e64
luisantonio16 <luis16.16m@gmail.com>
2023-04-22 12:30:48 -0400

    ss

--- src/Components/series.jsx
+++ src/Components/series.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import shazam from '../assets/shazam.jpeg'
 import superman from '../assets/superman1.jpg'
+import Fecht from './fecht.js'
+
 
 function series(){
+    const api = "https://api.themoviedb.org/3/trending/all/week?api_key=cdb5b34b6205cd62b1fb36acc98795e2";
+    const [datosApi, setdatos] = useState([])
+
+    useEffect(()=>{
+        fetch(api).then(res=>{
+            res.json().then(Datos=> setdatos(Datos.results))
+          
+        })
+    }, [])
+
+
     return(
         <section className="series">
-            <h2 className="series-tilte">Series</h2>
+            <h2 className="series-tilte">Los Mas Vistos</h2>
            <article className="series-container">
-               <div className="box">
-                   <div className="box-img">
-                       <img src={shazam} alt="Shazam" />
-                   </div>
-                   <div className="box-texts">
-                       <h1>Shazam fury the gods</h1>  
-                       <h2>Duracion: 2:40hrs</h2>
-                   </div>
-               </div>
+            {
+                datosApi.map(datos=>{
+                    return(
+                        <div className="box" key={datos.id}>
+                              <figure className="box-img">
+                                 <img src={datos.backdrop_path} alt="Shazam" />
+                              </figure>
+                             <div className="box-texts">
+                                 <h1>{datos.title}</h1>  
+                             </div>
+                             <div className="box-porcert">{datos.vote_average}</div>
+                        </div>
+                    );
+                })
+            }
+           
            </article>
         </section>  
     );
 }
 
 export default series;
+
+
+{/* <div className="box" key={datos.id}>
+<figure className="box-img">
+     <img src={datos.poster_path} alt="Shazam" />
+</figure>
+<div className="box-texts">
+     <h1>{datos.title}</h1>  
+     <h2>Duracion: 2:40hrs</h2>
+</div>
+<div className="box-porcert">{datos.vote_average}</div>
+</div> */}
