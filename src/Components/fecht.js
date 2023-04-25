b75f330ef5717ccbeb3cc0666121b01d0d417ef3
luisantonio16 <luis16.16m@gmail.com>
2023-04-24 20:46:25 -0400

    update

--- src/Components/fecht.js
+++ src/Components/fecht.js
@@ -1,11 +1,33 @@
-let datos =[]
-function fectSemanal() {
+function fectMovie() {
+    const datos = []
     fetch("https://api.themoviedb.org/3/trending/all/week?api_key=cdb5b34b6205cd62b1fb36acc98795e2")
     .then(res=>{
-        res.json().then(datos=>{
-            console.log(datos);
-        })
+        res.json().then(datos)
+        return console.log(datos)
+      
     })
-    return datos;
 }
-export default fectSemanal;
+export default fectMovie;
+
+
+{
+    datosApi.map(datos=>{
+        const {vote_average} = datos
+        return(
+           
+            
+                      <div className="box" key={datos.id}>
+                            <figure className="box-img">
+                                 <img src={api_image+datos.poster_path} alt={datos.title} />
+                            </figure>
+                            <div className="box-texts">
+                                 <h1>{datos.title}</h1> 
+                                  <p>Fecha de estreno:</p> 
+                                 <h2>{datos.release_date}</h2>  
+                            </div>
+                             <div className="box-porcert">{Math.ceil(vote_average)+"/10"} </div>
+                      </div>
+        
+        );
+    })
+}
